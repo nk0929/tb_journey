1879bf2ccf926278de78f9ec7161a0e86c9d7620
nk0929 <43422192+nk0929@users.noreply.github.com>
2018-11-21 21:45:56 +0900

    データ型違いによるエラー消し

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import SearchForm from './compornents/SearchForm';
 import './App.css';
+
 class App extends Component {
 
   render() {
--- src/compornents/SearchForm.js
+++ src/compornents/SearchForm.js
@@ -6,13 +6,15 @@ class SearchForm extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      startDate: '',
-      endDate: '',
-      value: ''
+      startDate: null,
+      endDate: null,
+      value: {},
+      textInput:''
     };
     this.handleChangeStart = this.handleChangeStart.bind(this);
     this.handleChangeEnd = this.handleChangeEnd.bind(this);
-    this.show = this.show.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleTextSubmit = this.handleTextSubmit.bind(this);
 
   }
 
@@ -26,43 +28,46 @@ class SearchForm extends React.Component {
       endDate: date
     });
   }
-  update (e) {
-    this.setState({
-      value: e.target.value
-    });
-  }
-  show (e) {
-    if(this.state.startDate === '' || this.state.endDate === ''|| this.state.value === ''){
+  handleChange (e) {
+   this.setState(
+     {textInput: e.currentTarget.value}
+   );
+ }
+  handleTextSubmit (e) {
+    if(!this.state.startDate || !this.state.endDate || !this.state.textInput){
       window.alert('全ての項目に入力してください');
-    }else {
+    }else{
       console.log(`チェックイン${this.state.startDate}`);
       console.log(`チェックアウト${this.state.endDate}`);
-      console.log(`目的地は${this.state.value}`);
+      console.log(`目的地は${this.state.textInput}`);
     }
     e.preventDefault()
   }
   render() {
     return (
     <div className="searchBox">
-    <p>検索フォーム</p>
-    <input type="text" value={this.state.value}  onChange={e => this.update(e)} /><br/>
-    <DatePicker placeholderText={'チェックイン'}
-    selected={this.state.startDate}
-    selectsStart
-    startDate={this.state.startDate}
-    endDate={this.state.endDate}
-    onChange={this.handleChangeStart} />
-    <br/>
-    <DatePicker placeholderText={'チェックアウト'}
-    selected={this.state.endDate}
-    selectsEnd
-    startDate={this.state.startDate}
-    endDate={this.state.endDate}
-    onChange={this.handleChangeEnd} />
-    <br/>
-    <form>
-    <input type="button" value="検索する" className="btn-default" onClick={this.show}/>
-    </form>
+      <h2>SearchBox</h2>
+      <div>
+        <input type="text" value={this.state.textInput} onChange={this.handleChange} /><br/>
+        <DatePicker placeholderText={'チェックイン'}
+          selected={this.state.startDate}
+          selectsStart
+          startDate={this.state.startDate}
+          endDate={this.state.endDate}
+          onChange={this.handleChangeStart} />
+        <br/>
+        <DatePicker placeholderText={'チェックアウト'}
+          selected={this.state.endDate}
+          selectsEnd
+          startDate={this.state.startDate}
+          endDate={this.state.endDate}
+          onChange={this.handleChangeEnd} />
+        <br/>
+        <form>
+        <input type="button" value="検索する" className="btn-default" onClick={this.handleTextSubmit}/>
+        </form>
+      </div>
+
     </div>
     );
   }
